791c9983a0ad23919bb1c6c3de4b0e3894f36d61
Ema Guillén <ema@guillen.com.ar>
2022-04-11 11:08:04 -0300

    cleaning after upstream merge

--- restart_containers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/admin-mds/datos-abiertos/docker-ckan
-/usr/local/bin/docker-compose -f /home/admin-mds/datos-abiertos/docker-ckan/docker-compose.yml stop
-/usr/local/bin/docker-compose -f /home/admin-mds/datos-abiertos/docker-ckan/docker-compose.yml up -d
